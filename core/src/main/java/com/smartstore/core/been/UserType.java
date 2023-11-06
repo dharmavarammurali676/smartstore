487fb2599c5c665e53af0e4369557020d9d8ff03
murali.dharmavaram <dharmavarammurali13@gmail.com>
2023-11-06 15:31:33 -0800

    [2] New Header Component Added

---
+++ core/src/main/java/com/smartstore/core/been/UserType.java
@@ -0,0 +1,17 @@
+package com.smartstore.core.been;
+
+public class UserType {
+
+    private String currentUserType;
+
+    public UserType() {
+    }
+
+    public String getCurrentUserType() {
+        return currentUserType;
+    }
+
+    public void setCurrentUserType(String currentUserType) {
+        this.currentUserType = currentUserType;
+    }
+}
